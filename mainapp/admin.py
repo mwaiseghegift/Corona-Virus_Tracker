fd54920505e953c059d716c2395889c72db34d39
Gateway47 <itsregalo047@gmail.com>
2021-02-21 18:11:45 +0300

    countries

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Country
 # Register your models here.
+
+admin.site.header="RETECH Corona Statistics"
+
+admin.site.register(Country)
--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
+
+class Country(models.Model):
+    name = models.CharField(max_length=200)
+    
+    def get_absolute_url(self):    
+        return reverse('mainapp:countries', kwargs={'pk': self.pk})
+    
+    def __str__(self):
+        return self.name
